77eea1a80c78857db16734d3c494d3d44f5a4d30
Алексей Знаменский <azum23@gmail.com>
2017-07-21 15:05:50 +0300

    001

--- Recur_01/Program.cs
+++ Recur_01/Program.cs
@@ -8,8 +8,25 @@ namespace Recur_01
 {
     class Program
     {
+        static int Factorial(int n, ref int result)
+        {
+            result *= n;
+            n--;
+            if (n>0)
+            {
+                Factorial(n,ref result);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            Factorial(n,ref result);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
